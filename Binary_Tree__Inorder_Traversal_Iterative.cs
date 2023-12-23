aa3f0d5cc7eff934f22a509765270d40781b5ba4
Andrew Tse <atse9288@gmail.com>
2023-12-23 10:23:42 -0800

    Create Binary_Tree__Inorder_Traversal_Iterative.cs
    Given the root of a binary tree, return the inorder traversal of its nodes' values.
    
     
    
    Example 1:
    
    
    Input: root = [1,null,2,3]
    Output: [1,3,2]
    Example 2:
    
    Input: root = []
    Output: []
    Example 3:
    
    Input: root = [1]
    Output: [1]
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 100].
    -100 <= Node.val <= 100
     
    
    Follow up: Recursive solution is trivial, could you do it iteratively?

---
+++ Binary_Tree__Inorder_Traversal_Iterative.cs
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> InorderTraversal(TreeNode root) {
+        List<int> list = new List<int>();
+        Stack<TreeNode> nodes = new Stack<TreeNode>();
+        
+        if(root == null) {
+            return list;
+        }
+        //We need two loops for the left and right sides of a node.
+        while(root != null || nodes.Count > 0) {
+            //We loop through node = node.left pushing the current root until null
+            while(root != null) {
+                nodes.Push(root);
+                root = root.left;
+            }
+            //When we do hit a null, we can pop the last pushed position and print
+            root = nodes.Pop();
+            list.Add(root.val);
+            //We only travel right when we know the current head's left has been explored
+            root = root.right;
+        }
+        return list;
+    }
+}
+
+/**
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+**/
